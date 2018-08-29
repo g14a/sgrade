d0ec2327005af2d44b74b997a46e02e62b4387ac
gowtham-munukutla <gowtham.m81197@gmail.com>
2018-08-29 08:55:08 +0530

    Edited.

--- parallelPrimes/parallelsieve.go
+++ parallelPrimes/parallelsieve.go
@@ -1,34 +1,31 @@
-// A concurrent prime sieve
-
 package main
 
 import "fmt"
 
 func Generate(ch chan<- int) {
 	for i := 2; ; i++ {
-		ch <- i // Send 'i' to channel 'ch'.
+		ch <- i
 	}
 }
 
-// removing those divisible by 'prime'.
 func Filter(in <-chan int, out chan<- int, prime int) {
 	for {
 		i := <-in // Receive value from 'in'.
 		if i%prime != 0 {
-			out <- i // Send 'i' to 'out'.
+			out <- i
 		}
 	}
 }
 
 func main() {
-	ch := make(chan int)
-	go Generate(ch)
+	input := make(chan int)
+	go Generate(input)
 	for i := 0; i < 2000; i++ {
-		prime := <-ch
+		prime := <-input
 		fmt.Println(prime)
-		ch1 := make(chan int)
-		go Filter(ch, ch1, prime)
-		ch = ch1
+		output := make(chan int)
+		go Filter(input, output, prime)
+		input = output
 	}
 
 	fmt.Println()
