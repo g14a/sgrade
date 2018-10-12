c3f615ccf35fd54c27728e4de008edbadff95ca2
gowtham-munukutla <gowtham.m81197@gmail.com>
2018-10-12 23:02:41 +0530

    pp[i].

--- parallelPrimes/gos/parallelPrime.go
+++ parallelPrimes/gos/parallelPrime.go
@@ -2,22 +2,28 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
 	"sync"
+	"strconv"
 )
 
 func main() {
+
 	mainChannel := make(chan int)
 
-	n := 100000
+	n := 10
 	go Generate(mainChannel, n)
 
 	primeChannel := make(chan int)
 
 	var wg sync.WaitGroup
 
+	primes := make([]int, 0)
+
 	go func() {
 		for i := range primeChannel {
-			fmt.Println(i)
+			primes = append(primes, i)
 		}
 	}()
 
@@ -27,7 +33,49 @@ func main() {
 	}
 
 	wg.Wait()
-	close(primeChannel)
+
+	sort.Ints(primes)
+
+	for i := range primes {
+		fmt.Println(primes[i])
+	}
+
+	max := 4
+
+	pp := PerfectPowers(primes, max)
+
+	for i := range pp {
+		fmt.Println(string(pp[i]))
+	}
+}
+
+func PerfectPowers(primes []int, maxPower int) []string {
+	perfectPowers := make([]string, 0)
+	primebuf := make([]int, 0)
+
+	for elem := range primes {
+		primebuf = append(primebuf, primes[elem])
+
+		for index := range primebuf {
+			prime := primebuf[index]
+			sum := 0
+
+			for i := 0; i < len(primebuf); i++ {
+				sum += primebuf[i]
+			}
+
+			fmt.Println("Sum =", sum)
+
+			for power := 2; power < maxPower+1; power++ {
+				if isPower(sum, power) {
+					s := strconv.FormatInt(int64(prime), 10) + ":" + strconv.FormatInt(int64(elem), 10) + " = " + strconv.FormatInt(int64(sum), 10) + " = " + strconv.FormatInt(int64(getNthroot(sum, power)), 10) + "**" + strconv.FormatInt(int64(power), 10)
+					perfectPowers = append(perfectPowers, s)
+				}
+			}
+		}
+	}
+
+	return perfectPowers
 }
 
 func isPrime(n int) bool {
@@ -56,4 +104,15 @@ func Generate(ch chan<- int, thresh int) {
 	for i := 2; i <= thresh; i++ {
 		ch <- i
 	}
+}
+
+func isPower(i, base int) bool {
+	diff := math.Mod(math.Pow(float64(i), 1/float64(base)), 1)
+
+	return diff == 0.0 || diff > 0.9999999
+}
+
+func getNthroot(a, b int) int {
+	s := math.Pow(float64(a), 1/float64(b))
+	return int(s)
 }
