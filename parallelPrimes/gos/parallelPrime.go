aee97f9710c147b046fa89382171e7eddf892b92
gowtham-munukutla <gowtham.m81197@gmail.com>
2018-10-13 03:00:29 +0530

    configured stuff in GoLand
    Signed-off-by: gowtham-munukutla <gowtham.m81197@gmail.com>

--- parallelPrimes/gos/parallelPrime.go
+++ parallelPrimes/gos/parallelPrime.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
 	"sync"
 )
 
 func main() {
+
 	mainChannel := make(chan int)
 
-	n := 100000
+	n := 100
 	go Generate(mainChannel, n)
 
 	primeChannel := make(chan int)
 
 	var wg sync.WaitGroup
 
+	primes := make([]int, 0)
+
 	go func() {
 		for i := range primeChannel {
-			fmt.Println(i)
+			primes = append(primes, i)
 		}
 	}()
 
@@ -27,7 +32,45 @@ func main() {
 	}
 
 	wg.Wait()
-	close(primeChannel)
+
+	sort.Ints(primes)
+
+	for i := range primes {
+		fmt.Println(i)
+	}
+
+	max := 10
+	pp := PerfectPowers(primes, max)
+
+	for i := range pp {
+		fmt.Println(i)
+	}
+
+	fmt.Println("dank")
+}
+
+func PerfectPowers(primes []int, maxPower int) []string {
+	perfectPowers := make([]string, 0)
+
+	for i := range primes {
+		for index := range primes {
+			prime := primes[index]
+			sum := 0
+
+			for i := 0; i < len(primes); i++ {
+				sum += primes[i]
+			}
+
+			for power := 2; power < maxPower+1; power++ {
+				if isPower(sum, power) {
+					s := string(prime) + ":" + string(primes[i]) + " = " + string(sum) + " = " + string(getNthroot(sum, power)) + "**" + string(power)
+					perfectPowers = append(perfectPowers, s)
+				}
+			}
+		}
+	}
+
+	return perfectPowers
 }
 
 func isPrime(n int) bool {
@@ -56,4 +99,15 @@ func Generate(ch chan<- int, thresh int) {
 	for i := 2; i <= thresh; i++ {
 		ch <- i
 	}
+}
+
+func isPower(i, base int) bool {
+	diff := math.Mod(math.Pow(float64(i), (1/float64(base))), 1)
+
+	return diff == 0.0 || diff > 0.9999999
+}
+
+func getNthroot(a, b int) int {
+	s := math.Pow(float64(a), 1/float64(b))
+	return int(s)
 }
