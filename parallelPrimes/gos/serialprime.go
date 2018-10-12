a40b229b653b62af4e93a45bbf8e5cd720020195
gowtham-munukutla <gowtham.m81197@gmail.com>
2018-10-12 13:33:48 +0530

    cleaned up.

---
+++ parallelPrimes/gos/serialprime.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n := 100000
+
+	for i := 0; i < n; i++ {
+		if isPrime(i) {
+			fmt.Println(i)
+		}
+	}
+}
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+
+	for i := 2; i <= n/2; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
