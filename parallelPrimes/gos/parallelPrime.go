cd0b5089d767c1d2c3bca22c68302b524f79a013
gowtham-munukutla <gowtham.m81197@gmail.com>
2018-10-13 01:23:32 +0530

    updated NthRoot and isPower

--- parallelPrimes/gos/parallelPrime.go
+++ parallelPrimes/gos/parallelPrime.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"math"
 	"sort"
-	"sync"
 	"strconv"
+	"sync"
 )
 
 func main() {
@@ -60,7 +60,7 @@ func PerfectPowers(primes []int, maxPower int) []string {
 			prime := primebuf[index]
 			sum := 0
 
-			for i := 0; i < len(primebuf); i++ {
+			for i := index; i < len(primebuf); i++ {
 				sum += primebuf[i]
 			}
 
@@ -68,7 +68,7 @@ func PerfectPowers(primes []int, maxPower int) []string {
 
 			for power := 2; power < maxPower+1; power++ {
 				if isPower(sum, power) {
-					s := strconv.FormatInt(int64(prime), 10) + ":" + strconv.FormatInt(int64(elem), 10) + " = " + strconv.FormatInt(int64(sum), 10) + " = " + strconv.FormatInt(int64(getNthroot(sum, power)), 10) + "**" + strconv.FormatInt(int64(power), 10)
+					s := strconv.FormatInt(int64(prime), 10) + ":" + strconv.FormatInt(int64(primes[elem]), 10) + " = " + strconv.FormatInt(int64(sum), 10) + " = " + strconv.FormatInt(int64(power), 10) + "**" + strconv.FormatInt(int64(getNthroot(sum, power)), 10)
 					perfectPowers = append(perfectPowers, s)
 				}
 			}
@@ -106,13 +106,19 @@ func Generate(ch chan<- int, thresh int) {
 	}
 }
 
-func isPower(i, base int) bool {
-	diff := math.Mod(math.Pow(float64(i), 1/float64(base)), 1)
+func isPower(num, base int) bool {
+	k := math.Log(float64(num)) / math.Log(float64(base))
 
-	return diff == 0.0 || diff > 0.9999999
+	t := math.Abs(k - math.Round(k))
+
+	if t < 0.0000001 {
+		return true
+	}
+	return false
 }
 
 func getNthroot(a, b int) int {
 	s := math.Pow(float64(a), 1/float64(b))
-	return int(s)
+
+	return int(math.Round(s))
 }
